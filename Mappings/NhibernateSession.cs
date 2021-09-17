6477988eacfb1884af800cc2529e2c040fbc7d13
ghilger16 <ghilger16@yahoo.com>
2021-09-17 15:43:16 -0700

    configure nhibernate

--- Mappings/NhibernateSession.cs
+++ Mappings/NhibernateSession.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting.Internal;
 using NHibernate;
 using NHibernate.Cfg;
-using ISession = NHibernate.ISession;
+
+
+
+
 
 namespace StockRocket.Mappings
 {
     public class NhibernateSession
     {
-        public static ISession OpenSession()
+        public static NHibernate.ISession OpenSession()
         {
             var configuration = new Configuration();
-            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\hibernate.cfg.xml");
+            var configurationPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,@"~\Models\hibernate.cfg.xml");
             configuration.Configure(configurationPath);
-            var bookConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Mappings\Book.hbm.xml");
+            var bookConfigurationFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"~\Mappings\Book.hbm.xml");
             configuration.AddFile(bookConfigurationFile);
             ISessionFactory sessionFactory = configuration.BuildSessionFactory();
             return sessionFactory.OpenSession();
